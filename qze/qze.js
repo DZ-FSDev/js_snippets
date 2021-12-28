2eafb912e21e07cf305be4249d0f021bef237f92
DZ-FSDev <85122787+DZ-FSDev@users.noreply.github.com>
2021-12-28 16:02:54 -0600

    Create qze.js

---
+++ qze/qze.js
@@ -0,0 +1,64 @@
+/*  Original Licensing Copyright
+ * 
+ *  qze.js
+ *  Copyright (C) 2021  DZ-FSDev
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Toggles the visibility of elements whose class matches the specified class.
+ * Visibility of the first toggle is applied to subsequent class members of
+ * the document.
+ *
+ * @param {String} clazz The specified class to find from the Document.
+ * @author DZ-FSDev
+ */
+function toggleVisibleByClass(clazz) {
+  var xx = document.getElementsByClassName(clazz);
+  var invisible = xx[0].style.display === "none";
+  for(let x of xx){
+  	if (invisible) {
+    	x.style.display = "block";
+  	} else {
+    	x.style.display = "none";
+ 	}
+  }
+}
+
+/**
+ * Makes visible the elements whose class matches the specified class.
+ *
+ * @param {String} clazz The specified class to find from the Document.
+ * @author DZ-FSDev
+ */
+function showVisibleByClass(clazz) {
+  var xx = document.getElementsByClassName(clazz);
+  for(let x of xx){
+    x.style.display = "block";
+  }
+}
+
+/**
+ * Makes invisible the elements whose class matches the specified class.
+ *
+ * @param {String} clazz The specified class to find from the Document.
+ * @author DZ-FSDev
+ */
+function hideVisibleByClass(clazz) {
+  var xx = document.getElementsByClassName(clazz);
+  for(let x of xx){
+    x.style.display = "none";
+  }
+}
