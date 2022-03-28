69d0ce4648b3dbe5155d0eca9e98f6ea98bf7cf4
DZ-FSDev <dz.uofm@gmail.com>
2022-03-28 15:51:05 -0500

    Update DZrsQ.js

--- RainbowPath/RainbowSquare/DZrsQ.js
+++ RainbowPath/RainbowSquare/DZrsQ.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", DZrsQ);
+document.addEventListener("DOMContentLoaded", DZrsQs);
 /**
  * 
  * @author DZ-FSDev
@@ -21,3 +22,33 @@ function DZrsQ(){
             $elem.appendChild($e);
         }
     }
+}
+
+/**
+ * 
+ * @author DZ-FSDev
+ * @version 0.0.1
+ */
+function DZrsQs(){
+    let $styles = document.createElement('style');
+    $styles.type = 'text/css';
+
+    $styles.textContent = 
+    `.RainbowSquare span{
+        animation-duration: 5s;
+        animation-iteration-count: infinite;
+        animation-name: RainbowSquare;
+        position: relative;
+    }
+    
+    @keyframes RainbowSquare {
+        0%   {color:red; left:0px; top:0px;}
+        20%  {color:yellow; left:20px; top:0px;}
+        40%  {color:greenyellow; left:20px; top:20px;}
+        60%  {color:cyan; left:0px; top:20px;}
+        80% {color:plum; left:0px; top:10px;}
+        99% {color:red; left:0px; top:0px;}
+    }`;
+
+    document.head.appendChild($styles);
+}
